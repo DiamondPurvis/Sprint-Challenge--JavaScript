49c55a7cb18ed3ae2d3f169c80cf622af24330e7
DiamondPurvis <purvisdiamond@gmail.com>
2019-01-26 13:47:32 -0500

    Created base constructor -cuboidmaker

--- challenges/prototypes.js
+++ challenges/prototypes.js
@@ -5,6 +5,11 @@
 /* == Step 1: Base Constructor ==
   Create a constructor function named CuboidMaker that accepts properties for length, width, and height
 */
+function CuboidMaker(properties) {
+  this.length = properties.length
+  this.width = properties.width
+  this.height = properties.height
+};
 
 
 /* == Step 2: Volume Method ==
